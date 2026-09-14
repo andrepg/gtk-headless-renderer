e192f5c0404ede12206243235d1981cdf62a7ed3
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-09-14 16:25:16 -0300

    feat(core): add error_codes to output

---
+++ src/error_codes.h
@@ -0,0 +1,24 @@
+// Copyright (C) 2026 apg
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef GTK_EMBEDDED_PREVIEW_ERROR_CODES_H
+#define GTK_EMBEDDED_PREVIEW_ERROR_CODES_H
+#include <stdlib.h>
+
+static int ERROR_TEMPLATE_INVALID = 1025;
+static int ERROR_TEMPLATE_NOT_FOUND = 1026;
+
+#endif //GTK_EMBEDDED_PREVIEW_ERROR_CODES_H
